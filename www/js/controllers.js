dd538c31e1868290e06fb89f03f4e087b49db82b
Kavita-M <kavitam@jetpat.com>
2017-12-03 18:39:54 +0530

    States and View Configured

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('app.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
@@ -41,16 +41,15 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+.controller('MyStocksCtrl', function($scope) {
+  $scope.myStocksArray = [
+      {ticker: "SPB"},
+      {ticker: "VROS"},
+      {ticker: "MT3"},
+      {ticker: "RIB"},
+      {ticker: "HOC"}
+    ];
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('StocksCtrl', function($scope, $stateParams) {
 });
